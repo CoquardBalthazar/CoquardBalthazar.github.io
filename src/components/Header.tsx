b102995f6930155869ef729155d7b8c65e2fff03
Balthazar Coquard <balthazar.coquard@gmail.com>
2026-06-10 15:16:59 +0200

    refactor - Mobile menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,10 @@
+import { useState } from 'react'
+
 function Header() {
-  return (
-    <header className="navbar">
+    const [isMenuOpen, setIsMenuOpen] = useState(false)
+    return (
+
+<header className="navbar">
       <div className="navbar-ctrl-brand">
         <a href="#home" className="logo boxhead">
           <span> &lt;</span>
@@ -9,26 +13,26 @@ function Header() {
         </a>
       </div>
 
-      <div className="menu-icon" id="menu-icon">
+      <div className="menu-icon" onClick={() => setIsMenuOpen(!isMenuOpen)}>
         <div className="bar1"></div>
         <div className="bar2"></div>
         <div className="bar3"></div>
       </div>
 
-      <div className="navbar-ctrl-items" id="navbar-ctrl-items">
-        <a className="navbar-hide" id="navbar-hide">
+      <div className={`navbar-ctrl-items ${isMenuOpen ? 'show' : ''}`} >
+        <a className={`navbar-hide ${isMenuOpen ? '' : 'hide'}`} onClick={() => setIsMenuOpen(false)}>
           CLOSE
         </a>
-        <a href="#about-me" className="navbar-item boxhead" id="nav-about-me">
+        <a href="#about-me" className="navbar-item boxhead" onClick={() => setIsMenuOpen(false)}>
           About me
         </a>
-        <a href="#skills" className="navbar-item boxhead" id="nav-skills">
+        <a href="#skills" className="navbar-item boxhead" onClick={() => setIsMenuOpen(false)}>
           Skills
         </a>
-        <a href="#projects" className="navbar-item boxhead" id="nav-projects">
+        <a href="#projects" className="navbar-item boxhead" onClick={() => setIsMenuOpen(false)}>
           My projects
         </a>
-        <button className="btn btn-secondary btn-lets-talk" id="btn-navbar">
+        <button className="btn btn-secondary btn-lets-talk" onClick={() => setIsMenuOpen(false)}>
           Let's talk
         </button>
       </div>
