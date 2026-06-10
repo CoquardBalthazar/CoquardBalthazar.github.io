a05a00cec014a887af8fe2fce6c49fc9226f265d
Balthazar Coquard <balthazar.coquard@gmail.com>
2026-06-10 16:24:10 +0200

    ReturnToTop refactro

--- src/components/ReturnToTop.tsx
+++ src/components/ReturnToTop.tsx
@@ -1,6 +1,25 @@
+import  { useState, useEffect } from 'react'
 function ReturnToTop() {
+    const [isVisible, setIsVisible] = useState(false)
+
+    useEffect( () => {
+        const handleScroll = () => {
+            setIsVisible(window.scrollY > 250)
+        }
+        window.addEventListener('scroll', handleScroll)
+        return () => {
+            window.removeEventListener('scroll', handleScroll)
+        }
+    }, [])
+
+    // Function to scroll to the top
+    const scrollToTop = () => {
+        window.scrollTo({top : 0, behavior : 'smooth'})
+    }
+
+
   return (
-    <div id="return-to-top" className="hidden">
+    <div id="return-to-top" className={isVisible ? 'visible' : ''} onClick={scrollToTop}>
       <img src="/assets/btn-return-top.svg" alt="Return to Top" />
     </div>
   )
